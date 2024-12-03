4157746592ee4a52f836210996af2bcc1526672e
jrbart <jorabart@gmail.com>
2024-12-02 21:22:22 -0600

    add placeholder profile page

--- lib/twittex/accounts.ex
+++ lib/twittex/accounts.ex
@@ -26,6 +26,10 @@ defmodule Twittex.Accounts do
     Repo.get_by(User, email: email)
   end
 
+  def get_user_by_username!(username) do
+    Repo.get_by!(User, username: username)
+  end
+
   @doc """
   Gets a user by email and password.
 
